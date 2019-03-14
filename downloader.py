32a4c038196c7ec02e3fe080970f289288d678f9
Max Du <maxdubusiness@gmail.com>
2019-03-14 17:15:21 -0400

    fixed some things wrong with the downaloders

--- downloader.py
+++ downloader.py
@@ -1,3 +1,3 @@
 from pytube import YouTube
 a = input("link? \n")
-YouTube(a).streams.first().download("C:/Users/wedu/Desktop/Downloaded Vids")
+YouTube(a).streams.first().download(output_path = 'C:/Users/wedu/Desktop/Downloaded Vids')
