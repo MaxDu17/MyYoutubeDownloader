0ff4a2e8a590ed5b976d9ce3082849d1446abc19
Maximilian Du <32785271+MaxDu17@users.noreply.github.com>
2020-12-24 15:11:18 -0500

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,3 +1,9 @@
-from pytube import YouTube
-a = input("link? \n")
-YouTube(a).streams.first().download(output_path = "G:/Desktop/Downloaded Vids")
+# from pytube import YouTube
+# a = input("link? \n")
+# YouTube(a).streams.first().download(output_path = "G:/Desktop/Downloaded Vids")
+import pytube
+
+print("Give URL:")
+url = input()
+
+pytube.YouTube(url).streams.get_highest_resolution().download('Video')
